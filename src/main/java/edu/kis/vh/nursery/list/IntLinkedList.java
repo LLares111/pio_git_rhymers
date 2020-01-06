1e2e75385f7ea2e8add36ffe6428cc537ba440b1
Bartosz Zalewski <bzalewski111@gmail.com>
2020-01-06 20:26:31 +0100

    Documentation added

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,10 +1,17 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Class representing integer linked list with nodes
+ */
 public class IntLinkedList {
 
     private Node last;
     private int i;
 
+    /**
+     * Method that pushes a given variable to the linked list
+     * @param i value to push to the list
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -15,20 +22,36 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * Method checking if the list is empty
+     * @return true if list is empty or true otherwise
+     */
     private boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * Method checking if the list is full
+     * @return false
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * Method returning last value in list
+     * @return -1 when it's empty or top value
+     */
     public int top() {
         if (isEmpty())
             return -1;
         return last.value;
     }
 
+    /**
+     * Method deleting last value from list and returning it's value
+     * @return -1 when it's empty or top value
+     */
     public int pop() {
         if (isEmpty())
             return -1;
