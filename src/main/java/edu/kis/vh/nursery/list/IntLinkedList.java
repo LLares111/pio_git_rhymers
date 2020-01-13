ddd94534c1c0d6a4d91b7c29250f0c6b6c287602
209476 <209476@edu.p.lodz.pl>
2020-01-13 19:42:50 +0100

    Wprowadzenie drobnych zmian

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,11 +1,18 @@
 package edu.kis.vh.nursery.list;
 
+
+
+ // Class representing integer linked list with nodes
 public class IntLinkedList {
 
 	public static final int NEGATIVE = -1;
 	Node last;
 	int I;
 
+    /**
+     * Method that pushes a given variable to the linked list
+     * @param i value to push to the list
+     */
 	public void push(int i) {
 		if (last == null)
 			last = new Node(i);
@@ -16,20 +23,38 @@ public class IntLinkedList {
 		}
 	}
 
+    /**
+     * Method checking if the list is empty
+     * @return true if list is empty or true otherwise
+     */
 	public boolean isEmpty() {
 		return last == null;
 	}
-
+	
+	/**
+     * Method checking if the list is full
+     */
 	public boolean isFull() {
 		return false;
 	}
+	
+	
 
+	 /**
+     * Method returning last value in list
+     * @return -1 when it's empty or top value
+     */
 	public int top() {
 		if (isEmpty())
 			return NEGATIVE;
 		return last.value;
 	}
 
+	
+    /**
+     * Method deleting last value from list and returning it's value
+     * @return -1 when it's empty or top value
+     */
 	public int pop() {
 		if (isEmpty())
 			return NEGATIVE;
